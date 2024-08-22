69a0105a8fbf6ce04f18cb494f885268d7af65fd
Manish Swain <manishswain@users.noreply.github.com>
2024-08-22 08:32:07 +0530

    Fixing a code

--- src/Array/CoPrime.java
+++ src/Array/CoPrime.java
@@ -1,33 +1,27 @@
 package Array;
+
 import java.util.*;
-class Main 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        List<Integer> list=new ArrayList<>();
-        for(int i=1;i<=n;i++)
-        {
-            
-            if(n%i==0)
-            {
-                 list.add(i);          
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+
+            if (n % i == 0) {
+                list.add(i);
             }
-        }      
-        int count=0;
-        for(int k=0;k<list.size()-1;k++)
-        {
-            for(int i=k+1;i<list.size();i++)
-            {
-                if(list.get(i)%list.get(k)!=0 )
-                {
+        }
+        int count = 0;
+        for (int k = 0; k < list.size() - 1; k++) {
+            for (int i = k + 1; i < list.size(); i++) {
+                if (list.get(i) % list.get(k) != 0) {
                     count++;
                 }
             }
         }
         System.out.println(count);
-        sc.close();  
+        sc.close();
     }
-    
 }
---
+++ src/Sigma/Practice/Practice2.java
@@ -0,0 +1,12 @@
+package Sigma.Practice;
+
+public class Practice2 {
+
+    public static void main(String[] args) {
+        int x = 2, y = 5;
+        int exp1 = (x * y / x);
+        int exp2 = (x * (y / x));
+
+        System.out.println(exp1 + " , " + exp2);
+    }
+}
