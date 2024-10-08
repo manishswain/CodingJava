10b5337c3d39188f234dcb261600249bb75feb56
Manish Swain <manishswain@users.noreply.github.com>
2024-10-08 22:53:07 +0530

    Patterns Practice Complete

--- src/Sigma/PatternAndPyramid/Butterfly.java
+++ src/Sigma/PatternAndPyramid/Butterfly.java
@@ -1,5 +1,34 @@
 public class Butterfly {
     public static void main(String[] args) {
-
+        int n = 8;
+        /* 1st half */
+        for (int i = 1; i <= n; i++) {
+            /* Stars */
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            /* Spaces */
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            /* Stars */
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        /* 2nd half */
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
---
+++ src/Sigma/PatternAndPyramid/SolidRombus.java
@@ -0,0 +1,14 @@
+public class SolidRombus {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
